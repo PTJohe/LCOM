86fafa733124d6e3134a0f84dbace2bec8b9dadb
up201303962 <up201303962@8034ce29-aede-45f2-b4ec-e1a5bd9fb3ea>
2015-09-29 15:27:55 +0000

    vt__print_int() completed

--- lab1/video_txt.c
+++ lab1/video_txt.c
@@ -10,83 +10,157 @@
 
 /* Private global variables */
 
-static char *video_mem;		/* Address to which VRAM is mapped */
+static char *video_mem; /* Address to which VRAM is mapped */
 
-static unsigned scr_width;	/* Width of screen in columns */
-static unsigned scr_lines;	/* Height of screen in lines */
+static unsigned scr_width; /* Width of screen in columns */
+static unsigned scr_lines; /* Height of screen in lines */
 
 void vt_fill(char ch, char attr) {
-  int k;
-  char *video_ptr;
-  video_ptr = video_mem;
-  
-  for (k = 0; k < scr_width * scr_lines; k++, video_ptr++){
-	  *video_ptr = ch; /* First byte, is for the character to display */
-	  video_ptr++; /* Point to Second byte of Video RAM*/
-	  *video_ptr = attr; /* Second byte, is for the attribute of character */
-  }
-  
+	int k;
+	char *video_ptr;
+	video_ptr = video_mem;
+
+	for (k = 0; k < scr_width * scr_lines; k++, video_ptr++) {
+		*video_ptr = ch; /* First byte, is for the character to display */
+		video_ptr++; /* Point to Second byte of Video RAM*/
+		*video_ptr = attr; /* Second byte, is for the attribute of character */
+	}
+
 }
 
 void vt_blank() {
 
-  char *video_ptr;
-  video_ptr = video_mem;
-  int j;
-  for (j = 0; j < scr_width * scr_lines; j++, video_ptr++){
-	  *video_ptr = 0x00;
-	  video_ptr++;
-	  *video_ptr = 0x00;
-  }
+	char *video_ptr;
+	video_ptr = video_mem;
+	int j;
+	for (j = 0; j < scr_width * scr_lines; j++, video_ptr++) {
+		*video_ptr = 0x00;
+		video_ptr++;
+		*video_ptr = 0x00;
+	}
 
+	//vt_fill(0x00, 0x00);
 
 }
 
 int vt_print_char(char ch, char attr, int r, int c) {
-  
+
 	char *video_ptr;
 	video_ptr = video_mem;
 	int i;
 	int row = r;
-	for (i = 0; i < row; i++){
-		video_ptr = video_ptr + scr_width*2;
+	for (i = 0; i < row; i++) {
+		video_ptr = video_ptr + scr_width * 2;
 	}
 	video_ptr = video_ptr + c * 2;
 
 	*video_ptr = ch;
 	video_ptr++;
 	*video_ptr = attr;
+
+	return 0;
 }
 
 int vt_print_string(char *str, char attr, int r, int c) {
+
 	int i, j;
 	int len = strlen(str);
 	char *video_ptr;
 	video_ptr = video_mem;
-	for (i = 1; i < r; i++){
-		video_ptr = video_ptr + scr_width*2;
+	for (i = 0; i < r; i++) {
+		video_ptr = video_ptr + scr_width * 2;
 	}
 	video_ptr = video_ptr + c * 2;
-	for (j = 0; j < len; j++, video_ptr++){
+	for (j = 0; j < len; j++, video_ptr++) {
 		*video_ptr = str[j];
 		video_ptr++;
 		*video_ptr = attr;
 	}
 
-
+	return 0;
 }
 
 int vt_print_int(int num, char attr, int r, int c) {
 
-  /* To complete ... */
+	char *video_ptr, ch;
+	video_ptr = video_mem;
+	video_ptr = video_ptr + (c * 2 + r * scr_width * 2);
+
+	int n = num;
+	int digits = 0;
+
+	//Count number of digits in the number
+	if (num > 0) {
+		while (n > 0) {
+			n /= 10;
+			digits++;
+		}
+	} else if (num < 0) {
+		n = abs(n);
+		num = abs(num);
+		while (n > 0) {
+			n /= 10;
+			digits++;
+		}
+		*video_ptr = '-';
+		video_ptr++;
+		*video_ptr = attr;
+		video_ptr++;
+	} else {
+		*video_ptr = '0';
+		video_ptr++;
+		*video_ptr = attr;
+		video_ptr++;
+		return 0;
+	}
+
+	video_ptr = video_ptr + (digits * 2);
+	for (digits; digits > 0; digits--) {
+		ch = (char) (((int) '0') + (num % 10));
+		video_ptr--;
+		*video_ptr = attr;
+		video_ptr--;
+		*video_ptr = ch;
+		num /= 10;
+	}
+	return 0;
 
 }
 
-
 int vt_draw_frame(int width, int height, char attr, int r, int c) {
+	//Checks if frame is within the screen dimensions
+	/*if ((c + width > scr_width) !! (r + height) > scr_height)) {
+		return 1;
+	}*/
 
-  /* To complete ... */
+	/*int i;
+	char *video_ptr;
+	video_ptr = video_mem;
+	video_ptr = video_ptr + (c * 2 + r * scr_width * 2);
 
+
+	//Draw upper left corner
+	*video_ptr = UL_CORNER;
+	video_ptr++;
+	*video_ptr = attr;
+	video_ptr++;
+
+	//Draw upper bar
+	for (i = 0; i < width; i++) {
+		*video_ptr = HOR_BAR;
+		video_ptr++;
+		*video_ptr = attr;
+		video_ptr++;
+	}
+
+	//Draw upper right corner
+	*video_ptr = UR_CORNER;
+	video_ptr++;
+	*video_ptr = attr;
+	video_ptr++;
+
+	return 0;
+	*/
 }
 
 /*
@@ -95,28 +169,28 @@ int vt_draw_frame(int width, int height, char attr, int r, int c) {
 
 char *vt_init(vt_info_t *vi_p) {
 
-  int r;
-  struct mem_range mr;
+	int r;
+	struct mem_range mr;
 
-  /* Allow memory mapping */
+	/* Allow memory mapping */
 
-  mr.mr_base = (phys_bytes)(vi_p->vram_base);
-  mr.mr_limit = mr.mr_base + vi_p->vram_size;
+	mr.mr_base = (phys_bytes)(vi_p->vram_base);
+	mr.mr_limit = mr.mr_base + vi_p->vram_size;
 
-  if( OK != (r = sys_privctl(SELF, SYS_PRIV_ADD_MEM, &mr)))
-	  panic("video_txt: sys_privctl (ADD_MEM) failed: %d\n", r);
+	if (OK != (r = sys_privctl(SELF, SYS_PRIV_ADD_MEM, &mr)))
+		panic("video_txt: sys_privctl (ADD_MEM) failed: %d\n", r);
 
-  /* Map memory */
+	/* Map memory */
 
-  video_mem = vm_map_phys(SELF, (void *)mr.mr_base, vi_p->vram_size);
+	video_mem = vm_map_phys(SELF, (void *) mr.mr_base, vi_p->vram_size);
 
-  if(video_mem == MAP_FAILED)
-	  panic("video_txt couldn't map video memory");
+	if (video_mem == MAP_FAILED)
+		panic("video_txt couldn't map video memory");
 
-  /* Save text mode resolution */
+	/* Save text mode resolution */
 
-  scr_lines = vi_p->scr_lines;
-  scr_width = vi_p->scr_width;
+	scr_lines = vi_p->scr_lines;
+	scr_width = vi_p->scr_width;
 
-  return video_mem;
+	return video_mem;
 }
