7165b2f229e235e4394f99abad2512436dc201b4
up201303962 <up201303962@8034ce29-aede-45f2-b4ec-e1a5bd9fb3ea>
2015-10-27 14:39:31 +0000

    Added keyboard.c and timer.c. Some changes in kbd_test_leds()

--- lab3/test3.c
+++ lab3/test3.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <minix/sysutil.h>
 
+#include "i8042.h"
 #include "test3.h"
 #include "keyboard.h"
 #include "timer.h"
@@ -64,21 +65,51 @@ int kbd_test_scan(unsigned short ass) {
 }
 
 int kbd_test_leds(unsigned short n, unsigned short *leds) {
-	int verify;
-	unsigned long stat = 0;
-	verify = kbd_subscribe_int();
-	if (verify == 1)
+	if (kbd_subscribe_int() == EXIT_FAILURE)
 		return EXIT_FAILURE;
+
+	unsigned long stat = 0;
 	unsigned int l;
+	int capsLock = 0;
+	int scrollLock = 0;
+	int numLock = 0;
+	int i = 0;
 	unsigned short (*arg) = 0;
-	for (l = 0; l < n; l++) {
-		if (leds[l] == 0 || leds[l] == 1 || leds[l] == 2)
-			*arg ^= leds[l];
-		printf("szfzdfg");
-		issue_commandArgument_KBC(KBD_TOGGLE_LEDS, *arg, stat);
-		timer_interrupt(1); // "The function kbd_test_leds() should process each element of the toggle array once per second"
+	/*for (l = 0; l < n; l++) {
+	 if (leds[l] == 0 || leds[l] == 1 || leds[l] == 2)
+	 *arg ^= leds[l];
+	 printf("szfzdfg");
+	 issue_commandArgument_KBC(KBD_TOGGLE_LEDS, *arg, stat);
+	 timer_interrupt(1); // "The function kbd_test_leds() should process each element of the toggle array once per second"
+	 }
+	 kbd_unsubscribe_int();*/
+
+	while (i < n) {
+		if (issue_commandArgument_KBC(KBD_TOGGLE_LEDS,
+				(scrollLock | numLock | capsLock) ^ BIT(leds[i]))
+				!= EXIT_SUCCESS) {
+			printf("ERROR: failed to write to KBD!");
+			return EXIT_FAILURE;
+		}
+		switch (leds[i]) {
+		case 0:
+			scrollLock = (scrollLock ^ BIT(0));
+			break;
+		case 1:
+			numLock = (numLock ^ BIT(1));
+			break;
+		case 2:
+			capsLock = (capsLock ^ BIT(2));
+			break;
+		default:
+			break;
+		}
+		i++;
+		timer_interrupt(1);
 	}
 	kbd_unsubscribe_int();
+	printf("kbd_test_leds concluido.\n");
+	return EXIT_SUCCESS;
 }
 
 int kbd_test_timed_scan(unsigned short n) {
--- lab3/timer.h
+++ lab3/timer.h
@@ -4,59 +4,11 @@
 static int timer_counter;
 static int hook_id_timer;
 
-int timer_subscribe_int(void) {
-	hook_id_timer = 0;
-	int bitmask = BIT(hook_id_timer);
-	if (sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hook_id_timer) == OK
-			&& sys_irqenable(&hook_id_timer) == OK)
-		return bitmask;
-	else
-		return EXIT_FAILURE;
-}
+int timer_subscribe_int(void);
 
-int timer_unsubscribe_int() {
+int timer_unsubscribe_int();
 
-	if (sys_irqrmpolicy(&hook_id_timer) != OK)
-		return EXIT_FAILURE;
-	else
-		return EXIT_SUCCESS;
-}
-
-int timer_interrupt(unsigned long time) {
-	if (time == 0) {
-		printf("Error: Time interval must be positive.\n");
-		return EXIT_FAILURE;
-	}
-
-	timer_counter = 0; // initialize "counter" global variable
-	int irq_set = timer_subscribe_int();
-	int interrupt = BIT(irq_set);
-	int r;
-	int ipc_status;
-	message msg;
-	while ((time * 60) >= timer_counter) { /* You may want to use a different condition */
-		/* Get a request message. */
-		if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
-			printf("driver_receive failed with: %d", r);
-			continue;
-		}
-		if (is_ipc_notify(ipc_status)) { /* received notification */
-			switch (_ENDPOINT_P(msg.m_source)) {
-			case HARDWARE: /* hardware interrupt notification */
-				if (msg.NOTIFY_ARG & interrupt) {
-					timer_counter += 1;
-				}
-				break;
-			default:
-				break; /* no other notifications expected: do nothing */
-			}
-		} else { /* received a standard message, not a notification */
-			/* no standard messages expected: do nothing */
-		}
-	}
-	timer_unsubscribe_int();
-	return EXIT_SUCCESS;
-}
+int timer_interrupt(unsigned long time);
 
 
 #endif
