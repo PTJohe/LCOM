6004e5cb32ec365d19c81e6142de76916fad2d1d
up201305602 <up201305602@8034ce29-aede-45f2-b4ec-e1a5bd9fb3ea>
2015-11-02 20:53:44 +0000

    lab4.c done

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -1,3 +1,126 @@
+#include <minix/drivers.h>
+#include "test4.h"
+
+static void print_usage(char *argv[]);
+static int proc_args(int argc, char *argv[]);
+static unsigned long parse_ulong(char *str, int base);
+static long parse_long(char *str, int base);
+
 int main(int argc, char *argv[]) {
-    /* To be completed ... */
+	/* Initialize service */
+	sef_startup();
+
+	/* Enable IO-sensitive operations for ourselves (used in lab3 to allow user level programs to executo I/O instructions)
+	sys_enable_iop(SELF);
+	 */
+
+	if (argc == 1) {
+		print_usage(argv);
+		return 0;
+	}
+	else {
+		proc_args(argc, argv);
+	}
+	return 0;
+}
+
+static void print_usage(char *argv[]) {
+	printf("Usage: one of the following:\n"
+			"\t service run %s -args \"packet <cnt>\" \n"
+			"\t service run %s -args \"async <timeout>\" \n"
+			"\t service run %s -args \"config <>\" \n"
+			"\t service run %s -args \"gesture <length> <tolerance>\" \n",
+			argv[0], argv[0], argv[0], argv[0]);
+}
+
+static int proc_args(int argc, char *argv[]){
+	unsigned short cnt, idle_time, tolerance;
+	short length;
+
+	 /* check the function to test: if the first characters match, accept it */
+	  if (strncmp(argv[1], "packet", strlen("packet")) == 0) {
+		  if( argc != 3 ) {
+			  printf("test_packet: wrong no of arguments for test of test_packet \n");
+			  return EXIT_FAILURE;
+		  }
+		  printf("test:: packet(%d)\n", (unsigned short) cnt);
+		  cnt = parse_ulong(argv[2], 16);
+		  if( cnt == ULONG_MAX || cnt < 1 )
+			  return EXIT_FAILURE;
+		  test_packet(cnt);
+		  return EXIT_SUCCESS;
+	  } else if (strncmp(argv[1], "async", strlen("async")) == 0) {
+		  if( argc != 3 ) {
+			  printf("test_async: wrong no of arguments for test of test_async() \n");
+			  return EXIT_FAILURE;
+		  }
+		  printf("test:: async(%d)\n", (unsigned short) idle_time);
+		  idle_time = parse_ulong(argv[2], 16)
+		  if( idle_time == ULONG_MAX )
+			  return EXIT_FAILURE;
+		  test_async(idle_time);
+		  return EXIT_SUCCESS;
+	  } else if (strncmp(argv[1], "config", strlen("config")) == 0) {
+		  if( argc != 2 ) {
+			  printf("test_config: wrong no of arguments for test of test_config() \n");
+			  return EXIT_FAILURE;
+		  }
+		  printf("test:: config()\n");
+		  test_config();
+		  return EXIT_SUCCESS;
+	  } else if (strncmp(argv[1], "gesture", strlen("gesture")) == 0) {
+		  if( argc != 4 ) {
+			  printf("test_gesture: wrong no of arguments for test of test_gesture() \n");
+			  return EXIT_FAILURE;
+		  }
+		  printf("test:: gesture(%d, %d)\n", (unsigned short) length, (unsigned short) tolerance);
+		  length = parse_ulong(argv[2], 10);
+		  tolerance = parse_ulong(argv[3], 10));
+		  if( length == ULONG_MAX )
+			  return EXIT_FAILURE;
+		  if( tolerance == ULONG_MAX )
+			  return EXIT_FAILURE;
+		  test_gesture(length, tolerance);
+		  return EXIT_SUCCESS;
+}
+
+static unsigned long parse_ulong(char *str, int base) {
+	char *endptr;
+	unsigned long val;
+
+	val = strtoul(str, &endptr, base);
+
+	if ((errno == ERANGE && val == ULONG_MAX) || (errno != 0 && val == 0)) {
+		perror("strtol");
+		return ULONG_MAX;
+	}
+
+	if (endptr == str) {
+		printf("kbd_test: parse_ulong: no digits were found in %s \n", str);
+		return ULONG_MAX;
+	}
+
+	/* Successful conversion */
+	return val;
+}
+
+static long parse_long(char *str, int base) {
+	char *endptr;
+	unsigned long val;
+
+	val = strtol(str, &endptr, base);
+
+	if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN))
+			|| (errno != 0 && val == 0)) {
+		perror("strtol");
+		return LONG_MAX;
+	}
+
+	if (endptr == str) {
+		printf("kbd_test: parse_long: no digits were found in %s \n", str);
+		return LONG_MAX;
+	}
+
+	/* Successful conversion */
+	return val;
 }
