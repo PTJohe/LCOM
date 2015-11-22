91bd3e0510bf59d945f2f34aa813ad12425e071c
up201305602 <up201305602@8034ce29-aede-45f2-b4ec-e1a5bd9fb3ea>
2015-11-22 17:31:03 +0000

    now added sprite.h and sprite.c. Forget so svn add

---
+++ lab5/sprite.c
@@ -0,0 +1,25 @@
+#include "sprite.h"
+#include "read_xpm.h"
+#include <stdlib.h>
+
+Sprite * create_sprite(char *pic[], char *base) {
+	//allocate space for the "object"
+	Sprite *sp = (Sprite *) malloc(sizeof(Sprite));
+	if (sp == NULL)
+		return NULL;
+	// read the sprite pixmap
+	sp->map = read_xpm(pic, &(sp->width), &(sp->height));
+	if (sp->map == NULL) {
+		free(sp);
+		return NULL;
+	}
+	return sp;
+}
+
+void destroy_sprite(Sprite *sp, char *base) {
+	if (sp == NULL)
+		return;
+	free(sp->map);
+	free(sp);
+	sp = NULL;
+}
